fedd2cc94acf2bce305dde9ff4b4d77a68a10244
Owen Jones <jonesor@gmail.com>
2022-12-28 00:16:10 +0100

    Update generateSchedule.R

--- code/generateSchedule.R
+++ code/generateSchedule.R
@@ -10,15 +10,64 @@
 library(lubridate)
 
 # Load data frame with event information
-events <- read.csv("events.csv")
+events <- read.csv("data/deepworkSchedule.csv")
+events
+
+#Date for next monday
+next_monday <- data.frame(date = seq(from = today(), by = "day", length.out = 7)) %>%
+  mutate(Day = wday(date, label = TRUE, abbr = FALSE)) %>%
+  filter(Day == "Monday") %>%
+  pull(date)
+
+# Create a data frame with the day of the week and date
+next_work_week <- data.frame(date = seq(from = next_monday, by = "day", length.out = 5)) %>%
+  mutate(Day = wday(date, label = TRUE, abbr = FALSE))
+
+events <- left_join(next_work_week,events) %>% 
+  mutate(date_time_t = parse_date_time(Start.Time,'%I:%M %p')) %>% 
+  mutate(year = year(date), month = month(date), day = day(date)) %>% 
+  mutate(date_time = update(date_time_t,year = year, month = month, day = day)) %>% 
+  select(date_time, Duration, Activity)
+
+head(events)
+
+library(stringr)
+
+parse_duration_string <- function(duration_string) {
+  # Extract the numeric value from the text string
+  duration_minutes <- as.numeric(str_extract(duration_string, "\\d+"))
+  
+  # Check for the presence of specific time units in the text string
+  if (str_detect(duration_string, "hour")) {
+    # Multiply the numeric value by 60 to convert it to minutes
+    duration_minutes <- duration_minutes * 60
+  } else if (str_detect(duration_string, "minutes")) {
+    # Leave the numeric value as is
+  } else {
+    # Set the numeric value to 0 if no time units are detected
+    duration_minutes <- 0
+  }
+  
+  # Return the numeric value in minutes
+  return(duration_minutes)
+}
+
+events$duration_mins <- sapply(events$Duration, parse_duration_string)
+events
+  
+
 
 # Open file for writing
-file <- file("calendar.ics", "w")
+file <- file("deepWorkSchedule.ics", "w")
 
 # Loop through events
 for (i in 1:nrow(events)) {
   # Generate iCalendar entry for event
-  ics <- generate_ics(events$start_time[i], events$duration[i], events$time_zone[i], events$title[i], events$location[i], events$recurrence_rule[i])
+  ics <- generate_ics(start_datetime = events$date_time[i], 
+                      duration = events$duration_mins[i],  
+                      time_zone = "Europe/Copenhagen", 
+                      title = events$Activity[i], 
+                      location = "NA", recurrence_rule = NULL)
 
   # Write iCalendar entry to file
   writeLines(ics, file)
