ef9e5618d007b2f9478e4fc0833b96072d2531e9
Owen Jones <jonesor@gmail.com>
2022-12-28 11:11:08 +0100

    Update generate_ics.R
    Added some input validation

--- code/generate_ics.R
+++ code/generate_ics.R
@@ -13,21 +13,39 @@
 #' @examples
 #' generate_ics("2022-12-27 15:00:00", 60, "America/New_York", "Meeting", "Conference Room")
 #' generate_ics("2022-12-27 15:00:00", 60, "America/New_York", "Meeting", "Conference Room", "FREQ=DAILY;COUNT=5")
-
+#'
 generate_ics <- function(start_datetime, duration, time_zone, title, location, recurrence_rule = NULL) {
-  
+
+  # Validation
+  # Check that duration is a positive numeric value
+  if (!is.numeric(duration) || !is.positive(duration)) {
+    stop("Error: duration must be a positive numeric value")
+  }
+
+  # Check that time_zone is a valid time zone identifier
+  if (!time_zone %in% OlsonNames()) {
+    stop("Error: time_zone must be a valid time zone identifier")
+  }
+
   # Convert time zone to tz object
   tz <- tz(time_zone)
-  
+
   # Convert start time to POSIXct object
-  start_time <- as.POSIXct(start_datetime, tz = tz)
-  
+  # start_time <- as.POSIXct(start_datetime, tz = tz)
+
+  # Check that start_datetime is a character string in the correct format
+  start_time <- strptime(start_datetime, format = "%Y-%m-%d %H:%M:%S", tz = tz)
+  if (is.na(start_time)) {
+    stop("Error: start_datetime must be a character string in the format YYYY-MM-DD HH:MM:SS")
+  }
+
+
   # Convert duration to seconds
   duration_secs <- duration * 60
-  
+
   # Calculate end time
   end_time <- start_time + duration_secs
-  
+
   # Define iCalendar template
   ics_template <- "BEGIN:VCALENDAR
 BEGIN:VEVENT
@@ -39,25 +57,29 @@ LOCATION:%s
 %s
 END:VEVENT
 END:VCALENDAR"
-  
+
   # Fill in template with input values
   if (is.null(recurrence_rule)) {
-    ics <- sprintf(ics_template,
-                   format(start_time, "%Y%m%dT%H%M%S"),
-                   format(end_time, "%Y%m%dT%H%M%S"),
-                   tz,
-                   title,
-                   location,
-                   "")
+    ics <- sprintf(
+      ics_template,
+      format(start_time, "%Y%m%dT%H%M%S"),
+      format(end_time, "%Y%m%dT%H%M%S"),
+      tz,
+      title,
+      location,
+      ""
+    )
   } else {
-    ics <- sprintf(ics_template,
-                   format(start_time, "%Y%m%dT%H%M%S"),
-                   format(end_time, "%Y%m%dT%H%M%S"),
-                   tz,
-                   title,
-                   location,
-                   paste0("RRULE:", recurrence_rule))
+    ics <- sprintf(
+      ics_template,
+      format(start_time, "%Y%m%dT%H%M%S"),
+      format(end_time, "%Y%m%dT%H%M%S"),
+      tz,
+      title,
+      location,
+      paste0("RRULE:", recurrence_rule)
+    )
   }
-  
+
   return(ics)
 }
