dbfb3e29fea41c2c72f305715ed824462f3fe60a
Owen Jones <jonesor@gmail.com>
2023-01-01 21:33:25 +0100

    Update generate_ics.R

--- code/generate_ics.R
+++ code/generate_ics.R
@@ -10,16 +10,23 @@
 #' @param recurrence_rule A character string representing the recurrence rule for the event in the format defined by the iCalendar specification.
 #' @param freebusy A character string of FREE or BUSY indicating if the event should be indicated as Free/Busy in Outlook.
 #' @return A character string containing the iCalendar entry in plain text format.
+#' 
+#' @importFrom TeachingDemos char2seed
+#' @importFrom lubridate tz
+#' @importFrom TeachingDemos char2seed
 #' @export
 #' @examples
 #' generate_ics("2022-12-27 15:00:00", 60, "America/New_York", "Meeting", "Conference Room")
 #' generate_ics("2022-12-27 15:00:00", 60, "America/New_York", "Meeting", "Conference Room", "FREQ=DAILY;COUNT=5")
 #'
 generate_ics <- function(start_datetime, duration, time_zone, title, location, recurrence_rule = NULL, freebusy = "BUSY") {
+  ics_random_seed_text <- paste0(start_datetime, duration, time_zone, title, location, recurrence_rule, freebusy)
+  ics_random_seed <- char2seed(ics_random_seed_text, set = FALSE)
+  set.seed(ics_random_seed)
 
   # Validation
   # Check that duration is a positive numeric value
-  if (!is.numeric(duration) || !duration>0) {
+  if (!is.numeric(duration) || !duration > 0) {
     stop("Error: duration must be a positive numeric value")
   }
 
@@ -27,9 +34,6 @@ generate_ics <- function(start_datetime, duration, time_zone, title, location, r
   if (!time_zone %in% OlsonNames()) {
     stop("Error: time_zone must be a valid time zone identifier")
   }
-
-  # Convert time zone to tz object
-  tz <- tz(time_zone)
 
   # Check that start_datetime is a character string in the correct format
   start_time <- strptime(start_datetime, format = "%Y-%m-%d %H:%M:%S", tz = tz)
@@ -59,14 +63,14 @@ LOCATION:%s
 %s
 X-MICROSOFT-CDO-BUSYSTATUS:%s
 END:VEVENT
- 
+
 END:VCALENDAR"
 
   # Fill in template with input values
   if (is.null(recurrence_rule)) {
     ics <- sprintf(
       ics_template,
-      paste(sample(x = c(letters,0:9, rep("-",2)),replace = FALSE,size = 25), collapse = ""),
+      paste(sample(x = c(letters, 0:9, rep("-", 2)), replace = FALSE, size = 25), collapse = ""),
       format(start_time, "%Y%m%dT%H%M%S"),
       format(end_time, "%Y%m%dT%H%M%S"),
       tz,
