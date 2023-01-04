07f10c8f43bb0c5cbdf4c67706b9c5436491fcde
Owen Jones <jonesor@gmail.com>
2023-01-04 10:38:18 +0100

    Update generateSchedule.R

--- code/generateSchedule.R
+++ code/generateSchedule.R
@@ -41,8 +41,7 @@ for (i in 1:nrow(events)) {
     duration = events$Duration[i],
     time_zone = "Europe/Copenhagen",
     title = events$Activity[i],
-    location = "Office", recurrence_rule = NULL, 
-    reminder_time = "5M",
+    location = "Office", recurrence_rule = NULL,
     freebusy = as.character(ifelse(grepl(pattern = "Deep|Lunch|Exercise", x = events$Activity[i])>0,"BUSY","FREE"))
   )
 
